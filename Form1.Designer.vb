6bdb25e2772a780ff8ccf99c9fb59d00acc14063
MartinAldkdkdj <65997389+MartinAldkdkdj@users.noreply.github.com>
2023-10-12 12:21:02 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        DataGridView1 = New DataGridView()
+        Label2 = New Label()
+        Label3 = New Label()
+        DataGridView2 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(DataGridView2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("CHICKEN Pie Height", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(206, 25)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(448, 31)
+        Label1.TabIndex = 0
+        Label1.Text = "MIGRACION DE DATOS MYSQL A SQL SERVER"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(56, 85)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(305, 29)
+        Button1.TabIndex = 1
+        Button1.Text = "CONECTAR CON LAS BASES DE DATOS"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(492, 85)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(305, 29)
+        Button2.TabIndex = 2
+        Button2.Text = "MIGRACION DE DATOS"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(56, 176)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.RowTemplate.Height = 29
+        DataGridView1.Size = New Size(802, 188)
+        DataGridView1.TabIndex = 3
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(56, 153)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(56, 20)
+        Label2.TabIndex = 4
+        Label2.Text = "MYSQL"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(56, 386)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(90, 20)
+        Label3.TabIndex = 6
+        Label3.Text = "SQL SERVER"
+        ' 
+        ' DataGridView2
+        ' 
+        DataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView2.Location = New Point(56, 409)
+        DataGridView2.Name = "DataGridView2"
+        DataGridView2.RowHeadersWidth = 51
+        DataGridView2.RowTemplate.Height = 29
+        DataGridView2.Size = New Size(802, 188)
+        DataGridView2.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(935, 644)
+        Controls.Add(Label3)
+        Controls.Add(DataGridView2)
+        Controls.Add(Label2)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(DataGridView2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView2 As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,101 @@
+﻿Imports System.Data.Odbc
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim mysqlconODBC As New OdbcConnection("dsn=pruebaOdbc; uid= root; pwd=;")
+        Dim dt As DataTable
+        Dim da As OdbcDataAdapter
+        Dim ds As New DataSet
+
+        Dim consulta As String = "SELECT * FROM productos"
+        Try
+
+            da = New OdbcDataAdapter(consulta, mysqlconODBC)
+            mysqlconODBC.Open()
+            da.Fill(ds)
+            mysqlconODBC.Close()
+            dt = ds.Tables(0)
+            DataGridView1.DataSource = dt
+            Label2.Text = "Conexion fue exitosa"
+        Catch ex As Exception
+            MsgBox("Error : " & ex.Message)
+        End Try
+
+
+        Dim sqlserverconODBC As New OdbcConnection("dsn=SQLSERVER; uid=Admin; pwd=Admin;")
+        Dim dt2 As DataTable
+        Dim da2 As OdbcDataAdapter
+        Dim ds2 As New DataSet
+
+        Dim consulta2 As String = "SELECT * FROM Producto"
+        Try
+
+            da2 = New OdbcDataAdapter(consulta2, sqlserverconODBC)
+            sqlserverconODBC.Open()
+            da2.Fill(ds2)
+            sqlserverconODBC.Close()
+            dt2 = ds2.Tables(0)
+            DataGridView2.DataSource = dt2
+            Label2.Text = "Conexion fue exitosa"
+        Catch ex As Exception
+            MsgBox("Error : " & ex.Message)
+        End Try
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+        Try
+            ' Consulta SQL para seleccionar datos de MySQL
+            Dim selectQuery As String = "SELECT * FROM Productos"
+
+            ' Conexión ODBC a MySQL
+            Using connectionMySQL As New OdbcConnection("dsn=pruebaOdbc; uid= root; pwd=;")
+                connectionMySQL.Open()
+
+                ' Comando para ejecutar la consulta en MySQL
+                Using cmdMySQL As New OdbcCommand(selectQuery, connectionMySQL)
+                    Using reader As OdbcDataReader = cmdMySQL.ExecuteReader()
+                        ' Conexión ODBC a SQL Server
+                        Using connectionSQLServer As New OdbcConnection("dsn=SQLSERVER; uid=Admin; pwd=Admin;")
+                            connectionSQLServer.Open()
+
+                            ' Consulta SQL para insertar datos en SQL Server
+                            Dim insertQuery As String = "INSERT INTO Producto (ID_Producto,Nombre_Producto,Descripcion,Precio_Unitario,Stock_Disponible,ID_Categoria) VALUES (?, ?, ?, ?, ?, ?)"
+
+                            ' Comando para insertar datos en SQL Server
+                            Using cmdSQLServer As New OdbcCommand(insertQuery, connectionSQLServer)
+                                While reader.Read()
+                                    ' Obtener valores de MySQL y asignarlos a parámetros en SQL Server
+                                    cmdSQLServer.Parameters.Clear()
+                                    cmdSQLServer.Parameters.AddWithValue("@param1", reader("ID_Producto"))
+                                    cmdSQLServer.Parameters.AddWithValue("@param2", reader("Nombre_Producto"))
+                                    cmdSQLServer.Parameters.AddWithValue("@param3", reader("Descripcion"))
+                                    cmdSQLServer.Parameters.AddWithValue("@param4", reader("Precio_Unitario"))
+                                    cmdSQLServer.Parameters.AddWithValue("@param5", reader("Stock_Disponible"))
+                                    cmdSQLServer.Parameters.AddWithValue("@param6", reader("ID_Categoria"))
+                                    ' Ejecutar la inserción en SQL Server
+                                    cmdSQLServer.ExecuteNonQuery()
+                                End While
+                            End Using
+                        End Using
+                    End Using
+                End Using
+            End Using
+
+            MessageBox.Show("Datos migrados exitosamente de MySQL a SQL Server.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show("Error al migrar datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+End Class
